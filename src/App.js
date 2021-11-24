09a3bcc0ea64e6a39c330aa5f626ef0c0c0e0657
Oskar Dragon <oskartdragon@gmail.com>
2021-11-24 21:44:11 +0000

    Add routes consants

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signin" element={<p>I will be a sign in page</p>} />
-      <Route path="/signup" element={<p>I will be a sign up page</p>} />
-      <Route path="/browse" element={<p>I will be the browse page</p>} />
+      <Route path={ROUTES.HOME} element={<Home />} />
+      <Route path={ROUTES.SIGN_IN} element={<p>I will be a sign in page</p>} />
+      <Route path={ROUTES.SIGN_UP} element={<p>I will be a sign up page</p>} />
+      <Route path={ROUTES.BROWSE} element={<p>I will be the browse page</p>} />
     </Routes>
   );
 }
