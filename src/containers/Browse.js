f4d160ce74ba405949d2edd3316ad42a22eb8dd1
Oskar Dragon <oskartdragon@gmail.com>
2021-11-29 21:49:58 +0000

    Add Firebase

--- src/containers/Browse.js
+++ src/containers/Browse.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Header } from "../components";
 import * as ROUTES from "../constants/routes";
-import { FirebaseContext } from "../context/useFirebase";
+import { FirebaseContext } from "../context/AuthContext";
 import { SelectProfileContainer } from "./Profiles";
 import { FooterContainer } from "./Footer";
 
@@ -9,7 +9,7 @@ export function BrowseContainer() {
   const [profile, setProfile] = useState({});
   const user = {
     displayName: "Oskar",
-    photoURL: "images/users/1.png",
+    photoURL: "images/users/2.png",
   };
 
   return profile.displayName ? (
