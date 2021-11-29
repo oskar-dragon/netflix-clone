4d4caa10e841ce298ef3ef168293ad9feca7bf82
Oskar Dragon <oskartdragon@gmail.com>
2021-11-29 21:47:56 +0000

    Add AuthProvider

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 import { Home, SignIn, SignUp, Browse } from "./pages";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <Routes>
-      <Route path={ROUTES.HOME} element={<Home />} />
-      <Route path={ROUTES.SIGN_IN} element={<SignIn />} />
-      <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
-      <Route path={ROUTES.BROWSE} element={<Browse />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path={ROUTES.HOME} element={<Home />} />
+        <Route path={ROUTES.SIGN_IN} element={<SignIn />} />
+        <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
+        <Route path={ROUTES.BROWSE} element={<Browse />} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
