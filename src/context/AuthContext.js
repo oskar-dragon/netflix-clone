0a9930ec3368a9c055f9d8dc9165c26be7f38545
Oskar Dragon <oskartdragon@gmail.com>
2021-11-30 21:37:42 +0000

    Add signIn to context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -5,14 +5,20 @@ const AuthContext = createContext();
 
 function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
 
   function signUp(email, password) {
     return auth.createUserWithEmailAndPassword(email, password);
   }
 
+  function signIn(email, password) {
+    return auth.signInWithEmailAndPassword(email, password);
+  }
+
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(user => {
       setCurrentUser(user);
+      setLoading(false);
     });
 
     return unsubscribe;
@@ -21,9 +27,14 @@ function AuthProvider({ children }) {
   const value = {
     currentUser,
     signUp,
+    signIn,
   };
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 }
 
 export { AuthProvider, AuthContext };
