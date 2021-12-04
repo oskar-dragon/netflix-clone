dcb5e8a06bfb6b7ef3c1032642c0031e9f447585
Oskar Dragon <oskartdragon@gmail.com>
2021-12-04 22:32:12 +0000

    Refactor firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,8 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
-import { seedDatabase } from "./seed";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-const app = firebase.initializeApp({
+const app = initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
@@ -12,6 +11,6 @@ const app = firebase.initializeApp({
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
-// seedDatabase(app);
+export const auth = getAuth();
+export const db = getFirestore(app);
 
-export const auth = app.auth();
