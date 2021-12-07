4abb8c97f680f869ea0bc42d3d0e6f14b1070ea4
Oskar Dragon <oskartdragon@gmail.com>
2021-12-07 20:52:16 +0000

    Add missing dependency

--- src/hooks/useContent.js
+++ src/hooks/useContent.js
@@ -23,7 +23,7 @@ export default function useContent(target) {
       .catch(error => {
         console.log(error.message);
       });
-  }, []);
+  }, [target]);
 
   return { [target]: content };
 }
