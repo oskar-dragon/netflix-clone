ab0b0ec4d917fc9273ee1011da0b340f8cc1d200
Oskar Dragon <oskartdragon@gmail.com>
2021-12-07 22:36:46 +0000

    Remove console.log

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -41,7 +41,6 @@ Accordion.Header = function AccordionHeader({ children, ...restProps }) {
 
 Accordion.Body = function AccordionBody({ children, ...restProps }) {
   const { toggleShow } = useContext(ToggleContext);
-  console.log(toggleShow);
 
   return toggleShow ? <Body {...restProps}>{children}</Body> : null;
 };
